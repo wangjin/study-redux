9f54f4fe326ead5f018922226884223acc0bc315
Jin Wang <wangjin-252@hotmail.com>
2019-08-27 16:08:32 +0800

    add antd & redux dependency

--- src/component/todo/ToDoList.js
+++ src/component/todo/ToDoList.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+const data = ['早上8点开会', '早上9点喝咖啡', '下午5点下班']
 class TodoList extends Component {
   render() {
-    return <div>Hello World</div>
+    return (
+      <div>
+        <div>
+          <Input placeholder="jspang" style={{ width: '250px' }} />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: '10px', width: '300px' }}>
+          <List
+            bordered
+            dataSource={data}
+            renderItem={item => <List.Item>{item}</List.Item>}
+          />
+        </div>
+      </div>
+    )
   }
 }
 export default TodoList
